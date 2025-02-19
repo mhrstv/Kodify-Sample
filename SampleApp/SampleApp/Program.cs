e2a5ea7d613fc9061a43f0c565b3e6242d234956
Martin Hristov <entrydev.gm@gmail.com>
2025-02-19 02:27:33 +0200

    Updated app to show usage of the library
    - Added comments
    - Updated to latest version

--- SampleApp/SampleApp/Program.cs
+++ SampleApp/SampleApp/Program.cs
@@ -1,24 +1,23 @@
 ﻿using Kodify.AI.Configuration;
 using Kodify.AI.Services;
 using Kodify.AutoDoc.Services;
+using Kodify.AutoDoc.Services.Documentation;
 
-var analyzer = new CodeAnalyzer();
+// Scan current project
+var analyzer = new ProjectAnalyzer();
 var projectInfo = analyzer.Analyze();
 
-var aiConfig = new OpenAIConfig
-{
-    ApiKey = "",
-    Model = "gpt-4o-mini"
-};
+// Initialize AI service (OpenAI)
+var aiConfig = new OpenAIConfig { ApiKey = "", Model = "gpt-4o-mini" };
 var aiService = new OpenAIService(aiConfig);
+
+// Create an instance of MarkdownGenerator (parsing our AI service to it)
 var markdownGenerator = new MarkdownGenerator(aiService);
 
+// Example usage of generation methods
 await markdownGenerator.GenerateReadMe(
     projectInfo,
     "Kodify Sample",
     "A .NET console app that showcases the usage of the Kodify .NET library.",
     "Install the app on your desired platform\nRun as you wish and test the usage of Kodify.");
-
-markdownGenerator.GenerateChangelog();
-
-// Changelog test
+await markdownGenerator.GenerateChangelogAsync(aiService);
