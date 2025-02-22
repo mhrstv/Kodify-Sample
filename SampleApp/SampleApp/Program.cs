86c942b24fe44004f541cecca6a7cb01b052bc12
Martin Hristov <entrydev.gm@gmail.com>
2025-02-23 01:30:16 +0200

    Update Program.cs

--- SampleApp/SampleApp/Program.cs
+++ SampleApp/SampleApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Kodify.AI.Configuration;
 using Kodify.AI.Services;
-using Kodify.AutoDoc.Services.Documentation;
+using Kodify.AutoDoc.Documentation;
+using Kodify.DevOps.IaC;
+using Kodify.DevOps.Pipeline;
 using Kodify.Extensions.Diagrams.Services;
 
 // Initialize AI service (OpenAI)
@@ -11,14 +13,26 @@ var aiConfig = new OpenAIConfig
 };
 var aiService = new OpenAIService(aiConfig);
 
-// Create instances of the services we will be using
+// Example usage of README and CHANGELOG generation methods
 var markdownGenerator = new MarkdownGenerator(aiService);
-var diagramGenerator = new PUMLDiagramGenerator();
-
-// Example usage of generation methods
 await markdownGenerator.GenerateReadMeAsync(
     "Kodify Sample",
     "A .NET console app that showcases the usage of the Kodify .NET library.",
     "Install the app on your desired platform\nRun as you wish and test the usage of Kodify.");
 await markdownGenerator.GenerateChangelogAsync(aiService);
+
+// Example usage of PUML generation
+var diagramGenerator = new PUMLDiagramGenerator();
 diagramGenerator.GeneratePUML();
+
+// Generates Terraform template
+var terraformGenerator = new TerraformGenerator();
+await terraformGenerator.GenerateTemplateAsync();
+
+// Generates AWS CloudFormation template
+var awsGenerator = new CloudFormationGenerator();
+await awsGenerator.GenerateTemplateAsync();
+
+// Automatically detects the type of pipeline needed and generates based on that
+var pipelineGenerator = Pipelines.DetectCI();
+await pipelineGenerator.GenerateAsync();
